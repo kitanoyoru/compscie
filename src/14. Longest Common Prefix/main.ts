6078f7b4d3b7a1de8541d2ff95611eba055b04c1
Alexandr Rutkovskij <kitanoyoru@protonmail.com>
2022-06-23 13:40:07 +0300

    9,14

---
+++ src/14. Longest Common Prefix/main.ts
@@ -0,0 +1,14 @@
+// 14.Longest Common Prefix solved by @kitanoyoru
+// https://leetcode.com/problems/longest-common-prefix/
+
+const longestCommonPrefix = (strs: string[]): string => {
+  let prefix: string = strs[0];
+  for (let i = 1; i < strs.length; ++i) {
+    for (let j = 0, k = 0; prefix[j] || strs[i][k]; ++j, ++k) {
+      if (prefix[j] != strs[i][k]) {
+        prefix = prefix.slice(0, j);
+      }
+    }
+  }
+  return prefix;
+}
---
+++ src/9. Palindrome Number/main.ts
@@ -0,0 +1,14 @@
+// 9. Palindrome Number solved by @kitanoyoru
+// https://leetcode.com/problems/palindrome-number/
+
+const isPalindrome = (x: number): boolean => {
+  return (
+    Number(
+      Array.from(x.toString())
+        .reverse()
+        .reduce((prev, curr) => prev + curr, "")
+    ) == x
+  );
+};
+
+console.log(isPalindrome(-121));
