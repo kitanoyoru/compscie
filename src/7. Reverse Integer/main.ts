07990a7898d2972ea4da6e6bb65cdb98878137f9
Alexandr Rutkovskij <kitanoyoru@protonmail.com>
2022-06-24 20:03:18 +0300

    200,7

---
+++ src/7. Reverse Integer/main.ts
@@ -0,0 +1,21 @@
+// 7. Reverse Integer solved by @kitanoyoru
+// https://www.postgresql.org/docs/current/sql-delete.html
+
+const reverse = (x: number): number => {
+  const reversedX = parseInt(
+    Array.from(Math.abs(x).toString())
+      .reverse()
+      .reduce((num, digit) => num + digit, "")
+  );
+  return x > 0
+    ? is32bit(reversedX)
+      ? reversedX
+      : 0
+    : is32bit(reversedX)
+    ? -1 * reversedX
+    : 0;
+};
+
+const is32bit = (num: number): boolean => {
+  return num >= -1 * Math.pow(2, 31) && num <= Math.pow(2, 31) - 1;
+};
