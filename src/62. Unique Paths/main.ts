3f211c65728b7573440d45fab23ca4a1d5ee0cd4
Alexandr Rutkovskij <kitanoyoru@protonmail.com>
2022-08-07 22:43:06 +0300

    solved some problems

---
+++ src/62. Unique Paths/main.ts
@@ -0,0 +1,21 @@
+// Solved by @kitanoyoru
+// https://leetcode.com/problems/unique-paths/
+
+const uniquePaths = (m: number, n: number) => {
+  if (m == 1 || n == 1) {
+    return 1
+  }
+
+  let dp = new Array(n).fill(0)
+  dp[0] = 1
+
+  for (let row = 0; row < m; row++) {
+    for (let col = 1; col < n; col++) {
+      dp[col] += dp[col - 1]
+    }
+  }
+
+  return dp[n - 1]
+}
+
+console.log(uniquePaths(3, 7))
