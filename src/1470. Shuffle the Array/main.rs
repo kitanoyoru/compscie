1bfd3f2b6414d5ec9f5e1fda3708c7130b381fd8
Alexandr Rutkovskij <kitanoyoru@protonmail.com>
2022-08-12 18:32:32 +0300

    solved some problems

---
+++ src/1470. Shuffle the Array/main.rs
@@ -0,0 +1,18 @@
+// Solved by @kitanoyoru
+// https://leetcode.com/problems/shuffle-the-array/
+
+impl Solution {
+    pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
+        let n = n as usize;
+        let mut ans = vec![0; nums.len()];
+        
+
+        for i in 0..n {
+            ans[2*i] = nums[i];
+            ans[2*i+1] = nums[i+n];
+        }
+        
+        ans
+    }
+}
+
