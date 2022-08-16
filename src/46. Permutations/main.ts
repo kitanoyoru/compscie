cf84be2b50a1fa501516cf4f7635fc64de2cafe1
Alexandr Rutkovskij <kitanoyoru@protonmail.com>
2022-08-16 12:17:25 +0300

    solved some problems

---
+++ src/46. Permutations/main.ts
@@ -0,0 +1,28 @@
+// Solved by @kitanoyoru
+// https://leetcode.com/problems/permutations/
+
+const permute = (nums: number[]): number[][] => {
+  let ans: number[][] = [];
+  let visited: boolean[] = new Array(nums.length).fill(false);
+
+  const dfs = (cur: number[]) => {
+    if (cur.length == nums.length) {
+      ans.push([...cur]);
+      return;
+    }
+    for (let i = 0; i < nums.length; i++) {
+      if (visited[i] == false) {
+        visited[i] = true;
+        cur.push(nums[i]);
+        dfs(cur);
+        cur.pop();
+        visited[i] = false;
+      }
+    }
+  };
+
+  dfs([]);
+
+  return ans;
+};
+
