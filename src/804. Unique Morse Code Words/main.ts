118ce7cbcdc0c5cfac92441d36ac64ad315d6712
Alexandr Rutkovskij <kitanoyoru@protonmail.com>
2022-08-17 22:25:34 +0300

    solved some problems

---
+++ src/804. Unique Morse Code Words/main.ts
@@ -0,0 +1,48 @@
+// Solved by @kitanoyoru
+// https://leetcode.com/problems/unique-morse-code-words/
+
+const MORSE_ALPH = [
+  ".-",
+  "-...",
+  "-.-.",
+  "-..",
+  ".",
+  "..-.",
+  "--.",
+  "....",
+  "..",
+  ".---",
+  "-.-",
+  ".-..",
+  "--",
+  "-.",
+  "---",
+  ".--.",
+  "--.-",
+  ".-.",
+  "...",
+  "-",
+  "..-",
+  "...-",
+  ".--",
+  "-..-",
+  "-.--",
+  "--..",
+];
+
+const morse = (word: string) => {
+  return word.split("").map((char) => MORSE_ALPH[char.charCodeAt(0) - 97]).join("")
+}
+
+const uniqueMorseRepresentations = (words: string[]): number => {
+  let s = new Set()
+  for (const word of words) {
+    const val = morse(word)
+    s.add(val)
+  }
+
+  return s.size
+}
+
+console.log(uniqueMorseRepresentations(["gin","zen","gig","msg"]));
+console.log(uniqueMorseRepresentations(["a"]));
