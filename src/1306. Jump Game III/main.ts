dfb4f9517de6ebd5c99dddf4fec7155fa617e7c9
Alexandr Rutkovskij <kitanoyoru@protonmail.com>
2022-08-18 13:15:31 +0300

    solved some problems

---
+++ src/1306. Jump Game III/main.ts
@@ -0,0 +1,32 @@
+// Solved by @kitanoyoru
+// https://leetcode.com/problems/jump-game-iii/
+
+const canReach = (arr: number[], start: number): boolean => {
+  let visited: boolean[] = new Array(arr.length).fill(false);
+  let q: number[] = [];
+
+  q.push(start);
+
+  while (q.length) {
+    let node = q.shift()!;
+    if (arr[node] == 0) {
+      return true;
+    }
+    if (!visited[node]) {
+      visited[node] = true;
+
+      let left = node + arr[node];
+      let right = node - arr[node];
+      
+      if (arr[left] || arr[left] == 0) {
+        q.push(left);
+      }
+      if (arr[right] || arr[right] == 0) {
+        q.push(right);
+      }
+    }
+  }
+
+  return false;
+};
+
