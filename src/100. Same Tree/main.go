2f3c134c6dc3b454d33a9fd1083584df27db9c68
Alexandr Rutkovskij <kitanoyoru@protonmail.com>
2022-10-24 00:33:59 +0300

    solved some problems

---
+++ src/100. Same Tree/main.go
@@ -0,0 +1,23 @@
+// Solved by @kitanoyoru
+// https://leetcode.com/problems/same-tree/
+
+package main
+
+type TreeNode struct {
+  Val int
+  Left *TreeNode
+  Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+  if p == nil && q == nil {
+    return true
+  }
+  if p == nil || q == nil {
+    return false
+  }
+  if p.val == q.val {
+    return isSameTree(q.Left, p.Left) && isSameTree(q.Right, p.Right)
+  }
+  return false
+}
