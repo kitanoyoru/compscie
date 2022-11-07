63ee3c2ea188bf38756f515cf0b2d1d07d46d7d1
Alexandr Rutkovskij <kitanoyoru@protonmail.com>
2022-11-07 16:21:19 +0300

    solved some problems

---
+++ src/386. Lexicographical Numbers/main.go
@@ -0,0 +1,23 @@
+// Solved by @kitanoyoru
+// https://leetcode.com/problems/lexicographical-numbers/
+
+func dfs(curr int, n *int, ans *[]int) {
+  if curr > *n {
+    return
+  }
+
+  *ans = append(*ans, curr)
+  for i := 0; i < 10; i++ {
+    dfs(curr * 10 + i, n, ans)
+  }
+}
+
+func lexicalOrder(n int) []int {
+  var ans []int = []int{}
+
+  for i := 1; i < 10; i++ {
+    dfs(i, &n, &ans)
+  }
+
+  return ans
+}
