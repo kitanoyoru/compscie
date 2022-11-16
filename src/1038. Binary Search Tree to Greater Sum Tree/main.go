00f7255edc8775a46a823e7a881af5d22ecce893
Alexandr Rutkovskij <kitanoyoru@protonmail.com>
2022-11-16 09:28:42 +0300

    solved some problems

---
+++ src/1038. Binary Search Tree to Greater Sum Tree/main.go
@@ -0,0 +1,26 @@
+// Solved by @kitanoyoru
+
+package main
+
+type TreeNode struct {
+  Val int
+  Left *TreeNode
+  Right *TreeNode
+}
+
+func dfs(node *TreeNode, nodeSum *int) {
+  if node.Right != nil {
+    dfs(node.Right, nodeSum)
+  }
+  *nodeSum += node.Val
+  node.Val = *nodeSum
+  if node.Left != nil {
+    dfs(node.Left, nodeSum)
+  }
+}
+
+func bstToGst(root *TreeNode) *TreeNode {
+  nodeSum := 0
+  dfs(root, &nodeSum)
+  return root
+}
