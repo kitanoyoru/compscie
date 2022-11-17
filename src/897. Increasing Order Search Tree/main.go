877857b0fd1c501ffd7a52a1ab353cbbc4c6f1c6
Alexandr Rutkovskij <kitanoyoru@protonmail.com>
2022-11-17 17:31:38 +0300

    solved some problems

---
+++ src/897. Increasing Order Search Tree/main.go
@@ -0,0 +1,23 @@
+// Solved by @kitanoyoru
+
+package main
+
+type TreeNode struct {
+  Val int
+  Left *TreeNode
+  Right *TreeNode
+}
+
+func helper(root, tail *TreeNode) *TreeNode {
+  if root == nil {
+    return tail
+  }
+  res := helper(root.Left, root)
+  root.Left = nil
+  root.Right = helper(root.Right, tail)
+  return res
+}
+
+func increasingBST(root *TreeNode) *TreeNode {
+  return helper(root, nil)
+}
