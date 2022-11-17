c9842bc859157f17eec684e8a42215b0f946cd1e
Alexandr Rutkovskij <kitanoyoru@protonmail.com>
2022-11-17 18:21:32 +0300

    solved some problems

---
+++ src/589. N-ary Tree Preorder Traversal/main.go
@@ -0,0 +1,29 @@
+// Solved by @kitanoyoru
+
+package main
+
+type Node struct {
+  Val int
+  Children []*Node
+}
+
+func helper(root *Node, arr *[]int) {
+  if root == nil {
+    return
+  }
+  for _, node := range root.Children {
+    *arr = append(*arr, node.Val)
+    helper(node, arr)
+  }
+}
+
+
+func preorder(root *Node) []int {
+  var arr []int
+  if root == nil {
+    return arr
+  }
+  arr = append(arr, root.Val)
+  helper(root, &arr)
+  return arr
+}
