1606b9455a05768d9451eda7fa6dac0dbf916872
Alexandr Rutkowski <kitanoyoru@protonmail.com>
2023-09-03 13:22:33 +0300

    solved some problems

---
+++ leetcode/62. Unique Paths/main.go
@@ -0,0 +1,26 @@
+package main
+
+func uniquePaths(m int, n int) int {
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+
+	dp[0][0] = 1
+
+	for i := 1; i < n; i++ {
+		dp[0][i] = 1
+	}
+	for j := 1; j < m; j++ {
+		dp[j][0] = 1
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] += dp[i-1][j] + dp[i][j-1]
+		}
+	}
+
+	return dp[m-1][n-1]
+
+}
