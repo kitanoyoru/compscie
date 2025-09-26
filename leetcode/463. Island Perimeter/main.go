2089f02b50bbe70c09c00369075fac20f15ce290
Alexandr Rutkowski <90549065+kitanoyoru@users.noreply.github.com>
2025-09-26 13:23:37 +0300

    Go (#22)

--- leetcode/463. Island Perimeter/main.go
+++ leetcode/463. Island Perimeter/main.go
@@ -1,21 +1,53 @@
 package main
 
-func islandPerimeter(grid [][]int) int {
-	lands, neigh := 0, 0
+type cell struct {
+	x, y int
+}
 
-	for i := range grid {
-		for j := range grid[i] {
-			if grid[i][j] == 1 {
-				lands += 1
-				if i < len(grid)-1 && grid[i+1][j] == 1 {
-					neigh++
-				}
-				if j < len(grid[i])-1 && grid[i][j+1] == 1 {
-					neigh++
-				}
+func islandPerimeter(grid [][]int) int {
+	if len(grid) == 0 || len(grid[0]) == 0 {
+		return 0
+	}
+
+	visited := make(map[cell]struct{})
+	directions := [][]int{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}
+
+	var perimeter int
+
+	var dfs func(y, x int)
+	dfs = func(y, x int) {
+		if y < 0 || y >= len(grid) || x < 0 || x >= len(grid[0]) {
+			return
+		}
+
+		if grid[y][x] == 0 {
+			return
+		}
+
+		if _, ok := visited[cell{x, y}]; ok {
+			return
+		}
+
+		visited[cell{x, y}] = struct{}{}
+
+		for _, d := range directions {
+			ny, nx := y+d[1], x+d[0]
+			if ny < 0 || ny >= len(grid) || nx < 0 || nx >= len(grid[0]) || grid[ny][nx] == 0 {
+				perimeter++
+			} else {
+				dfs(ny, nx)
 			}
 		}
 	}
 
-	return 4*lands - 2*neigh
+	for y := range len(grid) {
+		for x := range len(grid[0]) {
+			if grid[y][x] == 1 {
+				dfs(y, x)
+				break
+			}
+		}
+	}
+
+	return perimeter
 }
