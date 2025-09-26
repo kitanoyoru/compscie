19a9897a91d3fc918adb0ed907b62b3b8acff4b0
Alexandr Rutkowski <90549065+kitanoyoru@users.noreply.github.com>
2025-09-26 13:22:48 +0300

    494 (#28)
    * Go
    
    * Go

---
+++ leetcode/494. Target Sum/main.go
@@ -0,0 +1,19 @@
+package main
+
+func findTargetSumWays(nums []int, target int) int {
+	var backtrack func(idx, sum int) int
+	backtrack = func(idx, sum int) int {
+		if idx == len(nums) {
+			if sum == target {
+				return 1
+			}
+
+			return 0
+		}
+
+
+		return backtrack(idx+1, sum+nums[idx]) + backtrack(idx+1, sum-nums[idx])
+	}
+
+	return backtrack(0, 0)
+}
