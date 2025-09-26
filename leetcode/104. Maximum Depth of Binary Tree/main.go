7cf1d4e63004b09776faabf4f35cdc531370abb2
Alexandr Rutkowski <90549065+kitanoyoru@users.noreply.github.com>
2025-09-26 13:24:42 +0300

    Go (#15)

---
+++ leetcode/104. Maximum Depth of Binary Tree/main.go
@@ -0,0 +1,15 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
