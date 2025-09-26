7d79014bd341628e557f829d7e7505b43951050a
Alexandr Rutkowski <90549065+kitanoyoru@users.noreply.github.com>
2025-09-26 13:24:26 +0300

    Go (#17)

--- leetcode/111. Minimum Depth of Binary Tree/main.go
+++ leetcode/111. Minimum Depth of Binary Tree/main.go
@@ -1,6 +1,3 @@
-// Solved by @kitanoyoru
-// https://leetcode.com/problems/minimum-depth-of-binary-tree/
-
 package main
 
 type TreeNode struct {
@@ -9,22 +6,18 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
 func minDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
+
 	if root.Left == nil {
 		return 1 + minDepth(root.Right)
 	}
+
 	if root.Right == nil {
 		return 1 + minDepth(root.Left)
 	}
+
 	return 1 + min(minDepth(root.Left), minDepth(root.Right))
 }
