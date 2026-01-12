2c2a8f3f443daa64b8c15aa05fc8633b681a77ab
Alexandr Rutkowski <90549065+kitanoyoru@users.noreply.github.com>
2026-01-12 14:23:33 +0300

    Go (#56)

--- leetcode/712. Minimum ASCII Delete Sum for Two Strings/main.go
+++ leetcode/712. Minimum ASCII Delete Sum for Two Strings/main.go
@@ -1,17 +1,5 @@
 package main
 
-type Number interface {
-	int | int32 | int64 | float32 | float64
-}
-
-func min[T Number](a, b T) T {
-	if a < b {
-		return a
-	}
-
-	return b
-}
-
 func minimumDeleteSum(s1 string, s2 string) int {
 	dp := make([][]int, len(s1)+1)
 	for i := 0; i < len(s1)+1; i++ {
@@ -19,11 +7,11 @@ func minimumDeleteSum(s1 string, s2 string) int {
 	}
 
 	for i := 1; i < len(s1)+1; i++ {
-		dp[i][0] = dp[i-1][0] + int(s1[i])
+		dp[i][0] = dp[i-1][0] + int(s1[i-1])
 	}
 
 	for j := 1; j < len(s2)+1; j++ {
-		dp[0][j] = dp[0][j-1] + int(s2[j])
+		dp[0][j] = dp[0][j-1] + int(s2[j-1])
 	}
 
 	for i := 1; i < len(s1)+1; i++ {
@@ -31,10 +19,10 @@ func minimumDeleteSum(s1 string, s2 string) int {
 			if s1[i-1] == s2[j-1] {
 				dp[i][j] = dp[i-1][j-1]
 			} else {
-				dp[i][j] = min(dp[i][j-1]+int(s1[i-1]), dp[i-1][j]+int(s2[j-1]))
+				dp[i][j] = min(dp[i][j-1]+int(s2[j-1]), dp[i-1][j]+int(s1[i-1]))
 			}
 		}
 	}
 
-	return dp[len(s1)+1][len(s2)+1]
+	return dp[len(s1)][len(s2)]
 }
