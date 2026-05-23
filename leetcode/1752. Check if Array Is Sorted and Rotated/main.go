bbeada695a92ca5934e493d0c923e6dfca65ccd7
Alexandr Rutkowski <kitanoyoru@icloud.com>
2026-05-23 12:48:58 +0200

    Go

--- leetcode/1752. Check if Array Is Sorted and Rotated/main.go
+++ leetcode/1752. Check if Array Is Sorted and Rotated/main.go
@@ -1,15 +1,17 @@
 package main
 
 func check(nums []int) bool {
-	n := len(nums)
+	var (
+		count = 0
+		n = len(nums)
+	)
+
 	if n <= 1 {
 		return true
 	}
 
-	count := 0
-
-	for i := 0; i < n; i++ {
-		if nums[i] > nums[(i+1)%n] {
+	for i := range nums {
+		if nums[i] > nums[(i + 1) % n] {
 			count++
 		}
 	}
