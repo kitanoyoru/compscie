3b1f6d024070e1af44422ba5b8444afa412353ef
kitanoyoru <kitanoyoru@icloud.com>
2026-07-26 10:06:04 +0200

    Rust

---
+++ leetcode/628. Maximum Product of Three Numbers/main.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn maximum_product(mut nums: Vec<i32>) -> i32 {
+        nums.sort();
+
+        let n = nums.len();
+
+        let first = nums[0] * nums[1] * nums[n-1];
+        let second = nums[n-1] * nums[n-2] * nums[n-3];
+
+        if first > second { first } else { second }
+    }
+}
+
