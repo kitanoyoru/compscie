42164729fcb119a4f383238f8920b1fd685307e0
kitanoyoru <kitanoyoru@icloud.com>
2026-09-20 14:13:11 +0200

    Python

--- leetcode/medium/3498. Reverse Degree of a String/main.py
+++ leetcode/medium/3498. Reverse Degree of a String/main.py
@@ -1,8 +1,3 @@
 class Solution:
     def reverseDegree(self, s: str) -> int:
-        result = 0
-
-        for i, v in enumerate(s):
-            result += (i + 1) * (26 - (ord(v) - 97))
-
-        return result
+        return sum(((i + 1) * (26 - (ord(v) - 97)) for i, v in enumerate(s)))
